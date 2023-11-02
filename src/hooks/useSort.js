3301d4db8a3ff286ed30f3a386058749463e13d6
Daniil Siamenka <maloigb@gmail.com>
2023-11-02 12:16:22 +0300

    fixed-use-sort-sorting

--- src/hooks/useSort.js
+++ src/hooks/useSort.js
@@ -12,14 +12,30 @@ export default () => {
       setSortConfig({ ...sortConfig, fieldSort: field, directionSort: true });
     }
   };
+  const compareStrings = (a, b, directionSort) => {
+    if (directionSort) {
+      return String(a).localeCompare(String(b));
+    }
+    return String(b).localeCompare(String(a));
+  };
+
+  const compareNumbers = (a, b, directionSort) => {
+    if (directionSort) {
+      return a - b;
+    }
+    return b - a;
+  };
+
+  const sortTypeConfig = {
+    string: compareStrings,
+    number: compareNumbers,
+  };
   const sort = (field, data) => {
     changeSort(field);
     const sortedData = [...data];
     sortedData.sort((a, b) => {
-      if (sortConfig.directionSort) {
-        return String(a[field]).localeCompare(String(b[field]));
-      }
-      return String(b[field]).localeCompare(String(a[field]));
+      const fieldType = typeof a[field];
+      return sortTypeConfig[fieldType](a[field], b[field], sortConfig.directionSort);
     });
     return sortedData;
   };
